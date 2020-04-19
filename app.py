d902183d65d02c3fefaa4b20b3d55573de102d5b
Oliver Moeller <omoeller@uiowa.edu>
2020-04-19 17:03:48 -0500

    web db access

--- app.py
+++ app.py
@@ -6,19 +6,19 @@ from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import or_
 import pymysql
-import secrets
-#import os 
+#import secrets
+import os 
 
 
-#dbuser = os.environ.get('DBUSER')
-#dbpass = os.environ.get('DBPASS')
-#dbhost = os.environ.get('DBHOST')
-#dbname = os.environ.get('DBNAME')
+dbuser = os.environ.get('DBUSER')
+dbpass = os.environ.get('DBPASS')
+dbhost = os.environ.get('DBHOST')
+dbname = os.environ.get('DBNAME')
 
 
 
-conn ="mysql+pymysql://{0}:{1}@{2}/{3}".format(secrets.dbuser, secrets.dbpass, secrets.dbhost, secrets.dbname)
-#conn = "mysql+pymysql://{0}:{1}@{2}/{3}".format(dbuser, dbpass, dbhost, dbname)
+#conn ="mysql+pymysql://{0}:{1}@{2}/{3}".format(secrets.dbuser, secrets.dbpass, secrets.dbhost, secrets.dbname)
+conn = "mysql+pymysql://{0}:{1}@{2}/{3}".format(dbuser, dbpass, dbhost, dbname)
 
 
 
